2da3ccd0cb9da0a8e6e93cebdbc7898cb43cf188
weiwang94 <wei_wang94@sina.com>
2018-07-12 23:17:12 +0800

    初始化 Vue

---
+++ index.js
@@ -0,0 +1,12 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        message: 'Hello Vue!',
+        spanTitle: 'title',
+        todoList: [
+            {name: '吃饭'},
+            {name: '睡觉'},
+            {name: '打豆豆'},
+        ]
+    }
+})
